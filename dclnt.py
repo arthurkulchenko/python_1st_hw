f4df41c19aaddb276ba25189a268cc9a0296224e
Arthur <Artur.cr@pm.me>
2018-07-30 15:31:03 +0200

    cooperation of flattening methods, list and tuples

--- dclnt.py
+++ dclnt.py
@@ -3,44 +3,54 @@ import os
 import collections
 from nltk import pos_tag, word_tokenize, punkt
 
-Path = ''
-
+PATH = ''
+TOP_SIZE = 200
+PROJECTS = [
+                'django',
+                'flask',
+                'pyramid',
+                'reddit',
+                'requests',
+                'sqlalchemy',
+            ]
+# OPERATION AT LISTS
 def flattening(l):
-    for el in l:
-        if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
-            for sub in flattening(el):
-                yield sub
-        else:
-            yield el
-
-def flatten(array):
-    return [arr for arr in flattening(array)]
-
-def flattening_tuple(_list):
-    return list(sum(_list,()))
-
+    if type(l) == 'List':
+        for el in l:
+            if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+                for sub in flattening(el):
+                    yield sub
+            else:
+                yield el
+    else:
+        return list(sum(_list,()))
+# # OPERATION AT TUPLES
+# def flattening(_list):
+#     return list(sum(_list,()))
+# OPERATION AT STRINGS
 def is_verb(word = None):
     if word is None:
         return False
     else:
         pos_info = pos_tag(word_tokenize(word))
         return pos_info[0][1] in ('VB', 'VBD', 'VBZ', 'VBN')
-
-def only_py_extention(file, from_path):
+# FILTERING
+def filter_only_py_extention(file, from_path):
     if file.endswith('.py'):
         return os.path.join(from_path, file)
-
+# SEARCHING
 def find_py_files(from_path = Path):
     py_files_list = []
     for whole_path, dirs, files in os.walk(from_path, topdown = True):
-        for each_file in files:
-            py_files_list.append(only_py_extention(each_file, whole_path))
-            if len(py_files_list) == 100:
+        # OPTIMIZE py_files_list = [ filter_only_py_extention(each_file, whole_path) for each_file in files if len(py_files_list) <= 100 ]
+        for file in files:
+            py_files_list.append(filter_only_py_extention(file, whole_path))
+            if len(py_files_list) >= 100:
                 break
     py_files_list = filter(None, py_files_list)
     print('Total finded *.py files amount is: %s' % len(py_files_list))
     return py_files_list
-
+# SEARCHING
 def get_trees(with_files = False, with_file_content = False):
     files = find_py_files()
     trees = []
@@ -67,6 +77,7 @@ def get_verbs_from_function_name(function_name):
 # FILTER CHECKING IS A METHOT IS PRIVATE
 def is_private_filter_and_stringify(thing):
     if True:
+    # FIX no clearly stated conditions 
     # if thing.__class__.__name__.startswith('__') and thing.__class__.__name__.endswith('__'):
         # print thing.__class__.__name__
         return "%s" %thing.__class__.__name__
@@ -77,35 +88,30 @@ def is_astFunction_instance_filter(node):
 # FILTER FOR ARRAYS
 def is_none_filter(array):
     node_list = [map(is_astFunction_instance_filter(y), ast.walk(y)) for y in array]
-    return filter(None, node_list)
-# SCOPE METHOD 
-def get_top_verbs_in_path(top_size=10):
-    flatten_arr = flatten(is_none_filter(get_trees()))
-    fncs = [is_private_filter_and_stringify(f) for f in flatten_arr]
+    return filter(None, node_list) # OPTIMIZE <---------------------- need to delete somehow
+# should be called from get_common_verbs_across
+def get_common_verbs():
+    flatten_array = flattening(is_none_filter(get_trees()))
+    fncs = [is_private_filter_and_stringify(f) for f in flatten_array]
     fncs = filter(None, fncs)
     print('functions extracted')
-    verbs = flattening([get_verbs_from_function_name(function_name) for function_name in fncs])
-    return collections.Counter(verbs).most_common(top_size)
+    return flattening([get_verbs_from_function_name(function_name) for function_name in fncs])
 
-def some_wierd_stuff():
-    wds = []
-    projects = [
-                'django',
-                'flask',
-                'pyramid',
-                'reddit',
-                'requests',
-                'sqlalchemy',
-                ]
+# SCOPE METHOD 
+def the_most_common(objects, top_size=10):
+    return collections.Counter(objects).most_common(top_size)
 
-    for project in projects:
-        path = os.path.join('.', project)
-        wds += get_top_verbs_in_path(path)
+def get_common_verbs_across(projects, path):
+    # words = []
+    # for project in projects:
+    #     path = os.path.join('.', project)
+    #     words += get_common_verbs(path)
+    words = [ get_common_verbs() for words in projects]
 
-top_size = 200
-print('total %s words, %s unique' % (len(wds), len(set(wds))))
-for word, occurence in collections.Counter(wds).most_common(top_size):
-    print(word, occurence)
+    print('total %s words, %s unique' % (len(words), len(set(words))))
+    # for word, occurence in collections.Counter(words).most_common(top_size):
+    for word, occurence in the_most_common(words):
+        print(word, occurence)
 
 # UNUSED
 # 
@@ -114,10 +120,15 @@ for word, occurence in collections.Counter(wds).most_common(top_size):
 #
 # def split_snake_case_name_to_words(name):
 #     return [n for n in name.split('_') if n]
-#     return flattening_tuple([split_snake_case_name_to_words(function_name) for function_name in function_names])
+#     return flattening([split_snake_case_name_to_words(function_name) for function_name in function_names])
 
 # def get_top_functions_names_in_path(path, top_size=10):
 #     t = get_trees(path)
-#     nms = [f for f in flattening_tuple([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if not (f.startswith('__') and f.endswith('__'))]
+#     nms = [f for f in flattening([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if not (f.startswith('__') and f.endswith('__'))]
 #     return collections.Counter(nms).most_common(top_size)
+
+
+# UNNESSESARY
+# def flatten(array):
+#     return [arr for arr in flattening(array)]
 
