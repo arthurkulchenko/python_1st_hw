9972fb565f6f5f2e0fe39954f5cad3a34ec6c857
Arthur <Artur.cr@pm.me>
2018-08-01 11:43:09 +0200

    after flake8 lintering

--- custom_methods.py
+++ custom_methods.py
@@ -1,3 +1,10 @@
+import os
+import ast
+import collections
+from nltk import pos_tag, word_tokenize
+""", punkt"""
+
+
 def flattening(l):
     if type(l) == 'List':
         for el in l:
@@ -7,23 +14,32 @@ def flattening(l):
             else:
                 yield el
     else:
-        return list(sum(_list,()))
+        return list(sum(l, ()))
 
-def is_verb(word = None):
+
+def is_verb(word=None):
     if word is None:
         return False
     else:
         pos_info = pos_tag(word_tokenize(word))
         return pos_info[0][1] in ('VB', 'VBD', 'VBZ', 'VBN')
 
+
 def is_none_filter(array):
-    node_list = [map(is_ast_Function_instance_filter(y), ast.walk(y)) for y in array]
+    node_list = [map(is_astF_instance(y), ast.walk(y)) for y in array]
     return filter(None, node_list)
-    
+
+
 def filter_only_py_extention(file, from_path):
     if file.endswith('.py'):
         print file
         return os.path.join(from_path, file)
 
+
 def the_most_common(objects, top_size=10):
     return collections.Counter(objects).most_common(top_size)
+
+
+def is_astF_instance(node):
+    if isinstance(node, ast.FunctionDef):
+        return node.name.lower()
--- dclnt.py
+++ dclnt.py
@@ -1,26 +1,36 @@
 import ast
 import os
-import collections
-from nltk import pos_tag, word_tokenize, punkt
-import constants
+from constants import Path
 import custom_methods
-import unused
+
 
 def flattening(array):
     custom_methods.flattening(array)
 
-def is_verb(array = None):
+
+def is_verb(array=None):
     custom_methods.is_verb(array)
+
 
 def is_none_filter(array):
     custom_methods.is_none_filter(array)
 
+
 def filter_only_py_extention(file, from_path):
     custom_methods.filter_only_py_extention(file, from_path)
 
-def find_py_files(from_path = Path):
+
+def is_ast_Function_instance_filter(node):
+    custom_methods.is_ast_Function_instance_filter(node)
+
+
+def the_most_common(objects, top_size=10):
+    custom_methods.the_most_common(objects, top_size=10)
+
+
+def find_py_files(from_path=Path):
     files_list = []
-    for whole_path, dirs, files in os.walk(from_path, topdown = True):
+    for whole_path, dirs, files in os.walk(from_path, topdown=True):
         for file in files:
             files_list.append(filter_only_py_extention(file, whole_path))
             if len(files_list) >= 100:
@@ -29,7 +39,8 @@ def find_py_files(from_path = Path):
     print('Total finded *.py files amount is: %s' % len(files_list))
     return files_list
 
-def get_trees(files, with_files = False, with_file_content = False):
+
+def get_trees(files, with_files=False, with_file_content=False):
     trees = []
     for file in files:
         with open(file, 'r') as file_viewer:
@@ -48,35 +59,33 @@ def get_trees(files, with_files = False, with_file_content = False):
             trees.append(tree)
     print('trees generated')
     return trees
+
+
 def __test_method__():
     print "Hello"
 
+
 def is_private_filter_and_stringify(thing):
-    # FIX no clearly understood conditions 
+    # FIX no clearly understood conditions
     if thing.__class__.__name__.startswith('__') and thing.__class__.__name__.endswith('__'):
         print thing.__class__.__name__
-        return "%s" %thing.__class__.__name__
+        return "%s" % thing.__class__.__name__
 
-def is_ast_Function_instance_filter(node):
-    if isinstance(node, ast.FunctionDef):
-        return node.name.lower()
-
-def is_ast_Name_instance_filter(node):
-    if isinstance(node, ast.Name):
-        return node.name.lower()
 
 def get_common_verbs(trees):
     flatten_array = flattening(is_none_filter(trees))
     fncs = [is_private_filter_and_stringify(f) for f in flatten_array]
     fncs = filter(None, fncs)
     print('functions extracted')
-    generator = flattening([get_verbs_from_function_name(function_name) for function_name in fncs])
-    return list(generator)
+    g = flattening([getting_verbs(function_name) for function_name in fncs])
+    return list(g)
+
 
 def cascade_call(path):
     py_files = find_py_files(path)
     trees = get_trees(py_files)
     return get_common_verbs(trees)
+
 
 def get_common_verbs_across(projects):
     words = []
@@ -87,6 +96,6 @@ def get_common_verbs_across(projects):
     for word, occurence in the_most_common(words):
         print(word, occurence)
 
-def get_verbs_from_function_name(function_name):
-    return [word for word in function_name.split('_') if is_verb(word)]
 
+def getting_verbs(function_name):
+    return [word for word in function_name.split('_') if is_verb(word)]
